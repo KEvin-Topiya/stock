edb9d6e546aced9ecaa844a597c683e1e12f6506
KEvin-Topiya <kevin53846.914@gmail.com>
2026-03-02 21:27:38 +0530

    .

--- backend/main.go
+++ backend/main.go
@@ -90,7 +90,7 @@ func main() {
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Origin", "'https://stockgpt.jpassociate.co.in")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
