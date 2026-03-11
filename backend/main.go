c17353d85cda4c78155caf1ed540096e6132b962
KEvin-Topiya <kevin53846.914@gmail.com>
2026-03-12 02:26:44 +0530

    ..

--- backend/main.go
+++ backend/main.go
@@ -7,25 +7,30 @@ import (
 
 func niftyHandler(w http.ResponseWriter, r *http.Request) {
 
-	apiURL := "https://portal.tradebrains.in/api/index/constitients/NIFTY/?ascending=false&by=per_change&format=json&page=1&per_page=25"
+	url := "https://portal.tradebrains.in/api/index/constitients/NIFTY/?ascending=false&by=per_change&format=json&page=1&per_page=25"
 
-	resp, err := http.Get(apiURL)
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Set("User-Agent", "Mozilla/5.0")
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Referer", "https://portal.tradebrains.in/")
+	req.Header.Set("Origin", "https://portal.tradebrains.in")
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
 	if err != nil {
-		http.Error(w, "API request failed", http.StatusInternalServerError)
+		http.Error(w, "request failed", 500)
 		return
 	}
+
 	defer resp.Body.Close()
 
-	// set JSON header
 	w.Header().Set("Content-Type", "application/json")
-
-	// copy API response to client
 	io.Copy(w, resp.Body)
 }
 
 func main() {
-
 	http.HandleFunc("/api/nifty", niftyHandler)
-
 	http.ListenAndServe(":8080", nil)
 }
